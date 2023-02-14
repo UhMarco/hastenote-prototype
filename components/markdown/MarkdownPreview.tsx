328ce432b2c699050c6b653160a31afb13aee7e8
UhMarco <ddefygravity@gmail.com>
2023-02-14 14:03:07 +0000

    initial styling complete

--- components/markdown/MarkdownPreview.tsx
+++ components/markdown/MarkdownPreview.tsx
@@ -4,15 +4,39 @@ import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import rehypeRaw from "rehype-raw";
 import remarkGemoji from "remark-gemoji";
-import rehypeHighlight from "rehype-highlight";
-import rehypeMinifyWhitespace from "rehype-minify-whitespace";
+
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { coldarkDark as codeTheme } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 import styles from "./MarkdownPreview.module.css";
 
 export default function MarkdownPreview({ content = "" }: { content?: string; }) {
   return (
-    <div className={`p-12 text-white ${styles["markdown-body"]}`}>
-      <ReactMarkdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw, remarkGemoji, rehypeHighlight, rehypeMinifyWhitespace]}>{content}</ReactMarkdown>
+    <div className={`p-12 ${styles["markdown-body"]}`}>
+      <ReactMarkdown
+        remarkPlugins={[remarkGfm]}
+        rehypePlugins={[rehypeRaw, remarkGemoji]}
+        components={{
+          code({ node, inline, className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className || "");
+            return !inline && match ? (
+              <SyntaxHighlighter
+                language={match[1]}
+                PreTag="div"
+                // @ts-ignore
+                style={codeTheme}
+                {...props}
+              >{String(children).replace(/\n$/, "")}</SyntaxHighlighter>
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            );
+          }
+        }}
+      >
+        {content}
+      </ReactMarkdown>
     </div>
   );
 }
